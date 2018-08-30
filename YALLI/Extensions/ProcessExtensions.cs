ffdc9f39ac9903c594d3057ee42a4974212ab8d7
Daniel <daniel_belz@gmx.net>
2018-08-31 01:06:19 +0200

    LoadModule is now available through the static Injector class and through an extension for the process class

--- YALLI/Extensions/ProcessExtensions.cs
+++ YALLI/Extensions/ProcessExtensions.cs
@@ -1,40 +1,16 @@
 ﻿using System;
 using System.Diagnostics;
-using YALLI.Win32;
-using YALLI.Win32.Flags;
 
 namespace YALLI.Extensions
 {
     public static class ProcessExtensions
     {
-        internal static IntPtr OpenProcess(
-            this Process process,
-            ProcessAccess processAccess)
-        {
-            return Kernel32.OpenProcess(
-                processAccess,
-                false,
-                process.Id);
-        }
-
         public static IntPtr LoadModule(
             this Process process,
             string moduleName)
         {
-            Argument.IsNotNullOrWhitespace(
-                moduleName,
-                nameof(moduleName));
-
-            var hProcess = OpenProcess(
+            return Injector.LoadModule(
                 process,
-                ProcessAccess.PROCESS_CREATE_THREAD |
-                ProcessAccess.PROCESS_QUERY_INFORMATION |
-                ProcessAccess.PROCESS_VM_OPERATION |
-                ProcessAccess.PROCESS_VM_READ |
-                ProcessAccess.PROCESS_VM_WRITE);
-
-            return ProcessModulesHandler.LoadModule(
-                hProcess,
                 moduleName);
         }
 
@@ -42,16 +18,8 @@ namespace YALLI.Extensions
             this Process process,
             string moduleName)
         {
-            Argument.IsNotNullOrWhitespace(
-                moduleName,
-                nameof(moduleName));
-
-            var hProcess = OpenProcess(
+            return Injector.UnloadModule(
                 process,
-                ProcessAccess.PROCESS_ALL_ACCESS);
-
-            return ProcessModulesHandler.UnloadModule(
-                hProcess,
                 moduleName);
         }
     }
---
+++ YALLI/Injector.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using YALLI.Extensions;
+using YALLI.Win32;
+using YALLI.Win32.Flags;
+
+namespace YALLI
+{
+    public static class Injector
+    {
+        private const ProcessAccess DESIRED_PROCESS_ACCESS =
+            ProcessAccess.PROCESS_CREATE_THREAD |
+            ProcessAccess.PROCESS_QUERY_INFORMATION |
+            ProcessAccess.PROCESS_VM_OPERATION |
+            ProcessAccess.PROCESS_VM_READ |
+            ProcessAccess.PROCESS_VM_WRITE;
+
+        private static readonly IntPtr _loadLibraryProc;
+        private static readonly IntPtr _freeLibraryProc;
+        private static readonly int _charMarshalSize;
+
+        static Injector()
+        {
+            var kernel32ModuleHandle = Kernel32.GetModuleHandle(
+                "kernel32.dll");
+
+            _loadLibraryProc = Kernel32.GetProcAddress(
+                kernel32ModuleHandle,
+                "LoadLibraryA");
+
+            _freeLibraryProc = Kernel32.GetProcAddress(
+                kernel32ModuleHandle,
+                "FreeLibrary");
+
+            _charMarshalSize = Marshal.SizeOf<char>();
+        }
+
+        public static IntPtr LoadModule(
+            Process targetProcess,
+            string moduleName)
+        {
+            Argument.IsNotNull(
+                targetProcess,
+                nameof(targetProcess));
+
+            moduleName = NormalizeModuleName(
+                moduleName);
+
+            Argument.IsNotNullOrWhitespace(
+                moduleName,
+                nameof(moduleName));
+
+            var processHandle = GetProcessHandle(
+                targetProcess,
+                DESIRED_PROCESS_ACCESS);
+
+            if (processHandle.IsNULL())
+                return IntPtr.Zero;
+
+            var pArg = LoadArgument(
+                processHandle,
+                moduleName);
+
+            if (pArg.IsNULL())
+                return IntPtr.Zero;
+
+            return Kernel32
+                .CreateRemoteThread(processHandle, IntPtr.Zero, 0, _loadLibraryProc, pArg, 0, IntPtr.Zero);
+        }
+
+        public static bool UnloadModule(
+            Process targetProcess,
+            string moduleName)
+        {
+            throw new NotImplementedException();
+
+            Argument.IsNotNull(
+                targetProcess,
+                nameof(targetProcess));
+
+            moduleName = NormalizeModuleName(
+                moduleName);
+
+            Argument.IsNotNullOrWhitespace(
+                moduleName,
+                nameof(moduleName));
+
+            return true;
+        }
+
+        private static IntPtr LoadArgument(
+            IntPtr processHandle,
+            string argument)
+        {
+            try
+            {
+                var dwSize = new IntPtr(((argument.Length + 1) * _charMarshalSize));
+                var flAllocationType = AllocationType.Commit | AllocationType.Reserve;
+
+                var pAlloc = Kernel32.VirtualAllocEx(
+                    processHandle,
+                    IntPtr.Zero,
+                    dwSize,
+                    flAllocationType,
+                    MemoryProtection.ReadWrite);
+
+                if (pAlloc.IsNULL())
+                    return IntPtr.Zero;
+
+                byte[] buffer = Encoding.UTF8.GetBytes(argument);
+
+                Kernel32.WriteProcessMemory(
+                    processHandle,
+                    pAlloc,
+                    buffer,
+                    dwSize,
+                    out int bytesWritten);
+
+                return pAlloc;
+            }
+            catch { }
+
+            return IntPtr.Zero;
+        }
+
+        private static string NormalizeModuleName(
+            string moduleName)
+        {
+            if (string.IsNullOrWhiteSpace(moduleName))
+                return moduleName;
+
+            if (moduleName.EndsWith(".dll"))
+                return $"{moduleName}.dll";
+
+            return moduleName.ToUpper();
+        }
+
+        private static IntPtr GetProcessHandle(
+            Process process,
+            ProcessAccess desiredAccess)
+        {
+            return Kernel32.OpenProcess(
+                desiredAccess,
+                false,
+                process.Id);
+        }
+    }
+}
